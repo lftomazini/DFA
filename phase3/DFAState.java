3cf8c08fc51e1ba44c11115bd06dc674c8045b64
pobb001 <pobb001@bucknell.edu>
2015-10-29 18:40:16 -0400

    working on DFAState

---
+++ phase3/DFAState.java
@@ -0,0 +1,45 @@
+package phase3;
+import phase3.ExpTree.Operation;
+
+public class DFAState {
+    public ExpTree re;
+    public boolean halt;
+    public Tuple[] trans; 
+    public Alphabet alpha;
+    public Derivative deriv;
+    
+    public DFAState(ExpTree language) {
+        this.re = language;
+        this.halt = false;
+        this.trans = new Tuple[100];
+        this.alpha = new Alphabet();
+        this.deriv = new Derivative();
+        initialize();
+    }
+    
+    public void initialize() {
+        char[] a = this.alpha.getValues();
+        for(int i = 0 ; i < a.length ; i++) {
+            char letter = a[i];
+            DFAState newState = new DFAState(this.deriv.getDerivative(letter, this.re));
+            for(int j = i; j >= 0 ; j--) {
+                if(this.trans[j].tree.equals(newState.re)) this.trans[i] = this.trans[j].tree;
+                else this.trans[i] = new Tuple(letter, newState);
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        DFAState dfa = new DFAState(null);
+    }
+}
+
+class Tuple {
+    public char c;
+    public DFAState tree;
+    
+    public Tuple(char ch, DFAState t) {
+        this.c = ch;
+        this.tree = t;
+    }
+}
