1644f13285311593159aa2f74a47790d551caad0
Ethan Vynalek <ejv005@bucknell.edu>
2015-10-29 17:52:35 -0400

    adding REMatching

---
+++ phase3/REMatching.java
@@ -0,0 +1,9 @@
+package phase3;
+
+public class REMatching {
+    
+    // "When RE matches a string, we compute a derivative for each of the characters in the string"
+    
+    // "When RE does not match the string, we reach a derivative that is the RE {} and stop"
+    
+}
